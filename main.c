18411a0ae79eec1a2dc27aad6554500a81db7b61
Marradong <batty_et@icloud.com>
2022-09-07 11:08:09 +0800

    Initialised array

--- main.c
+++ main.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 #include "canvas.h"
 #include "toolbox.h"
+#include "verify.h"
 
-int main(int argc, char* argv[])
-{   
-    if(argc == 7)
+int main(int argc, char *argv[])
+{
+    if (vArgs(argc))
     {
-    int userInputs[6];
-    char** canvas = NULL;
+        int userInputs[6];
+        char **canvas;
 
-    charToInt(argv, userInputs, argc);
-    initArray(userInputs, canvas);
-    printCanvas(userInputs, canvas);
-    }
-    else
-    {
-        system("clear");
-        printf("Invalid number of arguements please enter in the format below\n");
-        printf("./escape <row_map> <col_map> <row_player> <col_player> <row_goal> <col_goal>\n");
+        charToInt(argv, userInputs, argc);
+        
+        initArray(userInputs, &canvas);
+        printCanvas(userInputs, canvas);
+
+        free(canvas);
     }
     return 0;
 }
